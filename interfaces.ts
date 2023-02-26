37412abe6b553c7be1610a7100aa662efda62da5
letier3110 <letier3110@gmail.com>
2023-02-26 10:19:22 +0400

    feat: update stroing data

---
+++ interfaces.ts
@@ -0,0 +1,42 @@
+export enum ActivityTypeEnum {
+  sleep = 'ActivityTypeEnum.sleep',
+  awake = 'ActivityTypeEnum.awake'
+}
+
+export enum FeedTypeEnum {
+  none = 'FeedTypeEnum.none',
+  right = 'FeedTypeEnum.right',
+  left = 'FeedTypeEnum.left',
+  both = 'FeedTypeEnum.both',
+  bottle = 'FeedTypeEnum.bottle'
+}
+
+export interface ActivityEntry {
+  activityType:ActivityTypeEnum;
+  date: string; // iso
+  duration: number; //seconds
+}
+  
+export interface FeedEntry {
+  feedType:FeedTypeEnum;
+  date: string; // iso
+  duration: number; //seconds
+}
+
+export interface UserStatsStorageEntry {
+  id: string; // slug foreign key?
+  activities: Array<ActivityEntry>;
+  feeds: Array<FeedEntry>;
+}
+
+export interface UserEntry {
+  id: string;
+  name: string;
+  babyname: string;
+  secret: string; // any type of auth/secret
+}
+
+export interface StorageEntry {
+  users: Array<UserEntry>;
+  userStats: Array<UserStatsStorageEntry>;
+}
